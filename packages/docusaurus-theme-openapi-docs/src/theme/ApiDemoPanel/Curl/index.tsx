56fefcb9dac03c29c741af5edb9e13e53bf31c7b
Joe Crop <joe.crop@gmail.com>
2023-07-08 20:43:38 -0700

    added ability to tak in custom code samples

--- packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/Curl/index.tsx
+++ packages/docusaurus-theme-openapi-docs/src/theme/ApiDemoPanel/Curl/index.tsx
@@ -277,7 +277,11 @@ function Curl({ postman, codeSamples }: Props) {
 
   return (
     <>
-      <CodeTabs groupId="code-samples" action={setLanguage}>
+      <CodeTabs
+        groupId="code-samples"
+        mergedLangs={mergedLangs}
+        action={setLanguage}
+      >
         {mergedLangs.map((lang) => {
           return (
             <CodeTab
